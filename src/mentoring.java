371a309062e3fb9433178f624b72c18411482e7d
Dilnoza <dilnoza.033@gmail.com>
2019-02-11 14:56:37 -0500

    add 4 sysout

--- src/mentoring.java
+++ src/mentoring.java
@@ -4,7 +4,7 @@ public class mentoring {
         System.out.println("Good luck"+"ok");
 
         System.out.println("everithing is good"+"ok");
-
+        System.out.println("Have a good day");
 
 
     }
