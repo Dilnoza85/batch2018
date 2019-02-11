34c2e6bf61bb91488f6c7a2c272499390e31fe63
Dilnoza85 <43626057+Dilnoza85@users.noreply.github.com>
2019-02-11 14:47:33 -0500

    add coma

--- src/mentoring.java
+++ src/mentoring.java
@@ -2,7 +2,7 @@ public class mentoring {
     public static void main(String[] args) {
         System.out.println("hello everybody");
         System.out.println("Good luck"+"ok");
-        System.out.println("everithing is good");
+        System.out.println("everithing is good",);
 
     }
 }
